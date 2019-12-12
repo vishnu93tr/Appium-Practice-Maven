21528b97bff140fadf92fbc20c007141e10e3b56
Ramachandran <ramachandran.p@renalteam.org>
2019-12-12 18:04:25 +0530

    completed toast messages automation

---
+++ src/test/java/Tests/ToastMessage.java
@@ -0,0 +1,29 @@
+package Tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class ToastMessage extends  Base
+{
+    @Test
+    public void  ToastMessages() throws MalformedURLException
+    {
+        AndroidDriver<AndroidElement> driver=Capabilities("emulator");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
+
+       String text= driver.findElementByXPath("//android.widget.Toast[1]").getAttribute("name");
+
+        System.out.println(text);
+
+        Assert.assertEquals(text,"Please enter your name","expected message and actual are not same");
+    }
+}
