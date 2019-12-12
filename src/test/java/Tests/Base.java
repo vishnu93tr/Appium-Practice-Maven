f35e27780693b1c37dbfdaba0bc80cb8b6ae607d
Ramachandran <ramachandran.p@renalteam.org>
2019-12-12 12:20:43 +0530

    maven project config

---
+++ src/test/java/Tests/Base.java
@@ -0,0 +1,45 @@
+package Tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.AndroidMobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class Base
+{
+    private static final String ACTIVITY = "com.androidsample.generalstore.MainActivity";
+    private static final String PACKAGE = "com.androidsample.generalstore";
+    private static AndroidDriver<AndroidElement> driver;
+
+    public static AndroidDriver<AndroidElement> Capabilities(String deviceName) throws MalformedURLException {
+
+            File f = new File("src");
+            File fs = new File(f, "General-Store.apk");
+            DesiredCapabilities capabilities = new DesiredCapabilities();
+            capabilities.setCapability("app", fs.getAbsolutePath());
+            capabilities.setCapability("autoGrantPermissions",true);
+            if(deviceName.equals("emulator"))
+            {
+                capabilities.setCapability("deviceName", "Emulator1");
+            }
+            else if(deviceName.equals("real"))
+            {
+                capabilities.setCapability("deviceName","Android Device");
+            }
+
+            capabilities.setCapability("automationName", "uiautomator2");
+            capabilities.setCapability(AndroidMobileCapabilityType.APP_PACKAGE, PACKAGE);
+            capabilities.setCapability(AndroidMobileCapabilityType.APP_WAIT_ACTIVITY, ACTIVITY);
+
+            driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+
+            System.out.println("setup successful");
+            return  driver;
+
+
+    }
+}
---
+++ src/test/java/Tests/Ecommerce_tc_1.java
@@ -0,0 +1,23 @@
+package Tests;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.annotations.Test;
+
+import java.net.MalformedURLException;
+import java.util.concurrent.TimeUnit;
+
+public class Ecommerce_tc_1 extends Base
+{
+    @Test
+    public void  test1() throws MalformedURLException
+    {
+        AndroidDriver<AndroidElement> driver=Capabilities("emulator");
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        WebDriverWait wait = new WebDriverWait(driver, 10);
+
+        driver.findElementById("com.androidsample.generalstore:id/nameField").sendKeys("Hello!");
+        driver.findElementById("com.androidsample.generalstore:id/btnLetsShop").click();
+    }
+}
